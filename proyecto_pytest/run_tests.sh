28406cdcea6d611498273db60c7b8e70c6ba0a81
unknown <danielreyes.16@homail.com>
2026-08-15 15:00:46 -0400

    Cambiando el run_tests.sh

--- proyecto_pytest/run_tests.sh
+++ proyecto_pytest/run_tests.sh
@@ -4,12 +4,23 @@ echo "Activando el entorno virtual"
 if [!"-d venv"]; then
     python3 -m venv venv
 fi
-source venv/bin/activate
+# Activar el entorno virutal correctamente
+if [ -f "venv/binb/activate" ]; then
+    source venve/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then
+    source venv/Scripts/activate
+else 
+    echo "Error: No se pudo activar el entorno virtual."
+    exit 1
+fi
 
-echo "Instalando dependencias"
-pip install -r requirements.txt
+# Verificar si `pip` esta instalado correctamente
+echo "Instalando dependencias ..."
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
-echo "Ejecuntaod pruebas con Pytest" 
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+# Ejecutar las pruebas
+echo "Ejecutando pruebas con Pytest" 
+venv/bin/python -m pytest tests/ --junitxml=reports/test/results.xml --html=reports/report.html --self-con
 
 echo "Pruebas finalizadas, resultados en reports"
